e4f372ef4fce7570d196fcf36475708494a56f44
yanlp <85827444@qq.com>
2020-03-20 11:33:32 +0800

    actuator微服务信息完善

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContextConfig.java
@@ -16,7 +16,7 @@ import org.springframework.web.client.RestTemplate;
 public class ApplicationContextConfig {
 
     @Bean
-    @LoadBalanced
+    @LoadBalanced // #LoadBalanced注解开启RestTemplate负载均衡功能
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
