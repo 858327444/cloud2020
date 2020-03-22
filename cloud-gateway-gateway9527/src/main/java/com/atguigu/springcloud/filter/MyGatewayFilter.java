2424401dce5661714d6d84bec2d62afd6aa4bdd3
yanlp <85827444@qq.com>
2020-03-22 22:39:50 +0800

    Global之自定义filter

---
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/filter/MyGatewayFilter.java
@@ -0,0 +1,39 @@
+package com.atguigu.springcloud.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * Program Name: cloud2020
+ * Created by yanlp on 2020-03-22
+ *
+ * @author yanlp
+ * @version 1.0
+ */
+@Component
+@Slf4j
+public class MyGatewayFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        // 例如 http://localhost:9527/payment/lb?uname=123456,这种请求可以访问进来
+        String uname = exchange.getRequest().getQueryParams().getFirst("uname");
+        if (StringUtils.isBlank(uname)) {
+            log.info("用户ing为空,o(╥﹏╥)o");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
