a1fbdf1261970888abbc2b3ed45f878ca38ee19d
yanlp <85827444@qq.com>
2020-03-22 21:22:57 +0800

    Gateway 9527工程搭建

---
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/GatewayMain9527.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Program Name: cloud2020
+ * Created by yanlp on 2020-03-22
+ *
+ * @author yanlp
+ * @version 1.0
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain9527.class,args);
+    }
+}
