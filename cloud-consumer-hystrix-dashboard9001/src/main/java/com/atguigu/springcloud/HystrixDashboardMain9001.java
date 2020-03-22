2803caced07a178e9109166d389f3690e0c14abf
yanlp <85827444@qq.com>
2020-03-22 18:29:34 +0800

    Hystrix 图形化,dashboard搭建

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/atguigu/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * Program Name: cloud2020
+ * Created by yanlp on 2020-03-22
+ *
+ * @author yanlp
+ * @version 1.0
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
