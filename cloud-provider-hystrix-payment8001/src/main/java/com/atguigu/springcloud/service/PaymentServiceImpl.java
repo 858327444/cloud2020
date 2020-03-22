b03880fb77d94d2213759428ff499cd08b2d6221
yanlp <85827444@qq.com>
2020-03-22 11:18:51 +0800

    Hystrix 之服务降级 支付测 fallback,完善注释

--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentServiceImpl.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentServiceImpl.java
@@ -20,14 +20,25 @@ public class PaymentServiceImpl implements PaymentService {
         return "线程名字: "+ Thread.currentThread().getName() + " paymentInfoOK  id:" + id;
     }
 
+    /**
+     * @HystrixCommand 报异常后如何处理:
+     * 一但调用服务方法失败并抛出了错误信息后
+     * 会自动调用@HystrixCommand标注好的fallbackMethod 调用类中的指定方法
+     *
+     * @param id
+     * @return
+     */
     @Override
     @HystrixCommand(fallbackMethod = "paymentInfoTimeoutHandler",commandProperties = {
-            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="500")
+            //设置这个线程的超时时间是3s，3s内是正常的业务逻辑，超过3s调用fallbackMethod指定的方法进行处理
+            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="3000")
     })
     public String paymentInfoTimeout(Integer id) {
         int timeNumber = 5;
         try {
             TimeUnit.SECONDS.sleep(timeNumber);} catch (InterruptedException e) {e.printStackTrace();}
+        // 程序就算写出bug,也会调用fallbackMethod 中的方法
+//        int age = 10 / 0;
 
         return  "线程名字: "+ Thread.currentThread().getName() + " id: "+ id + " paymentInfoTimeout ,耗时(秒):" + timeNumber;
     }
